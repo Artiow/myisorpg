0d25c39d7610396847566a1f5c703875d0fc67a4
Artem Namednev <artem.namednev@gmail.com>
2025-03-30 17:13:50 +0400

    New player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -275.0
-const JUMP_LIMIT = 2
+const SPEED: float = 150.0
+const JUMP_VELOCITY: float = -275.0
+const JUMP_LIMIT: int = 2
+
 
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var raycast: RayCast2D = $RayCast2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var kill_timer: Timer = $KillTimer
 
-  
-var jump_count = 0
+
+var jump_count: int = 0
+var dead: float = false
 
 
 func _physics_process(delta: float) -> void:
@@ -28,7 +30,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
+	if direction and not dead:
 		velocity.x = direction * SPEED
 		if direction > 0 and sprite.flip_h:
 			sprite.set_deferred("flip_h", false)
@@ -36,6 +38,15 @@ func _physics_process(delta: float) -> void:
 			sprite.set_deferred("flip_h", true)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if not dead:
+		if is_on_floor():
+			if direction:
+				sprite.call_deferred("play", "run")
+			else:
+				sprite.call_deferred("play", "idle")
+		elif sprite.animation != "jump":
+			sprite.call_deferred("play", "jump")
 
 	move_and_slide()
 
@@ -48,6 +59,7 @@ func drop_process() -> void:
 
 func jump_process() -> void:
 	if Input.is_action_just_pressed("jump") and can_jump():
+		sprite.call_deferred("play", "jump")
 		velocity.y = JUMP_VELOCITY
 		jump_count += 1
 	elif is_on_floor():
@@ -55,13 +67,17 @@ func jump_process() -> void:
 
 
 func can_jump() -> bool:
-	return is_on_floor() or (jump_count < JUMP_LIMIT)
+	return not dead and (is_on_floor() or (jump_count < JUMP_LIMIT))
 
 
 func kill() -> void:
-	Engine.time_scale = 0.5
-	collision.queue_free()
-	kill_timer.start()
+	if not dead:
+		dead = true
+		Engine.time_scale = 0.5
+		velocity.y = JUMP_VELOCITY / 2
+		sprite.call_deferred("play", "death", 2)
+		collision.queue_free()
+		kill_timer.start()
 
 
 func _on_kill_timer_timeout() -> void:
